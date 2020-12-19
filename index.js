f473ae549d842aebfd68b219aab17c75e1ab1877
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-12-19 01:22:47 -0500

    added Intial Docs

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.send({hi: "there"});
+    res.send({naz: "wright"});
 }
 );
 // should not be changed lightly
 // dynamic port binding
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+// host name 
+// https://cryptic-plateau-14392.herokuapp.com/
+// git repo deployment target 
+// https://git.heroku.com/cryptic-plateau-14392.git
 
