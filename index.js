2de08c08e86e9e98b5c3625abeea9d21bd750326
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2020-12-26 14:53:47 -0500

    second production commit

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
-// root file 
-
-// require gets access to express library
-const express = require('express'); 
-// generates new express application
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-//passport js google auth
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
-// referenced with authenticate()
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done)=>{
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
-    }
-));
-
-//google route
-// authenticate using the google passport strategy
-app.get( '/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email'] // add contacts possibly for contacts
-}));
-
-app.get('/auth/google/callback',
-    passport.authenticate('google')
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-app.get('/', (req,res) =>{
-    res.send({naz: "wriight"});
-});
-// dynamic port binding
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
 
 // host name 
 // https://cryptic-plateau-14392.herokuapp.com/
