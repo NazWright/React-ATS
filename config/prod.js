b3357dc4e0ba7e474e2fe4cb2f78de6bb9caeade
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2021-01-20 10:51:36 -0500

    clientside

--- config/prod.js
+++ config/prod.js
@@ -4,7 +4,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.eny.STRIPE_PUBLISHABLE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   GOLD_PRICE_ID: process.env.GOLD_PRICE_ID, // havent added to heroku yet
   SILVER_PRICE_ID: process.env.SILVER_PRICE_ID, // havent added to heroku yet
