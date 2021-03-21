674e0bb66ea4616e3d75ae960994be29b9f0bdd2
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2021-03-20 23:22:02 -0400

    Created test case for creating a listing by id

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -1,29 +1,8 @@
 const mongoose = require("mongoose");
-const User = mongoose.model("users");
 const Listing = mongoose.model("listings");
 
-describe("Listings Controller", () => {
-  let jobEmployer;
-
-  //   it("POSTS to /api/listings creates a new job listing", async () => {
-  //     jobEmployer = await new User({
-  //       googleId: "114983407291327089129",
-  //       familyName: "Wright",
-  //       givenName: "Nazere",
-  //       email: "nxwright@aggies.ncat.edu",
-  //       password: null,
-  //       role: "Admin",
-  //       isAdmin: true,
-  //       parent: null,
-  //       cust_Id: "cus_IlbKdIZxTyrSKW",
-  //       subscription: null,
-  //       accounts: 1,
-  //     })
-  //       .save()
-  //       .catch((err) => {
-  //         console.warn(err);
-  //       });
-
-  //     assert( )
-  //   });
-});
+module.exports = {
+  async create(req, res) {
+    const { title, location, jobinfo } = req.body;
+  },
+};
